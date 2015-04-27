babc477fcb11f717155255300a07cad886b26d20
yayugu <yayugu@gmail.com>
2015-04-27 11:10:42 +0900

    fix

--- lib/main.rb
+++ lib/main.rb
@@ -82,6 +82,6 @@ module Main
     title
       .gsub(/\s/, '_')
       .gsub(/\//, '_')
-      .slice(0, 200) # safe length for filename
+      .byteslice(0, 200).scrub('') # safe length for filename
   end
 end
