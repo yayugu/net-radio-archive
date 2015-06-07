655c250516764063b0872dd39f7c0de9555815cc
yayugu <yayugu@gmail.com>
2015-06-07 22:30:22 +0900

    fix

--- lib/niconico_live/downloading.rb
+++ lib/niconico_live/downloading.rb
@@ -121,7 +121,7 @@ module NiconicoLive
           @log_buffer << "downloaded file is not valid: #{@l.id}, #{full_file_path} but continue other file download"
           next
         end
-        mp4_path = full_file_path.gsub(/\.f4v$/, '') + '.mp4'
+        mp4_path = full_file_path.gsub(/\.flv$/, '') + '.mp4'
         Main::convert_ffmpeg_to_mp4(full_file_path, mp4_path, @program)
         Main::move_to_archive_dir(CH_NAME, @l.opens_at, mp4_path)
         succeed_count += 1
