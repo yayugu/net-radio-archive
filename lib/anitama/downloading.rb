23683a800061b6187cdef2a232d00ff0ddc8bd60
yayugu <yayugu@gmail.com>
2015-06-08 16:50:02 +0900

    fix

--- lib/anitama/downloading.rb
+++ lib/anitama/downloading.rb
@@ -3,7 +3,7 @@ module Anitama
     CH_NAME = 'anitama'
 
     def download(program)
-      Main::prepare_woking_dir(CH_NAME)
+      Main::prepare_working_dir(CH_NAME)
       path = filepath(program)
       Anitama::Scraping.new.download(program.book_id, path)
       Main::move_to_archive_dir(CH_NAME, program.update_time, path)
