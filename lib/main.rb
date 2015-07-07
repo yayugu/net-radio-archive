e828d8e498d7209103261a8e98dad9e9fe0c5183
t-ashula <office@ashula.info>
2015-07-07 16:37:29 +0900

    use fullwidth chars for windows

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'fileutils'
 require 'net/http'
 
@@ -122,6 +123,14 @@ module Main
     title
       .gsub(/\s/, '_')
       .gsub(/\//, '_')
+      .gsub(/\?/, "？") # \/:*?"<>|
+      .gsub(/\\/, "￥")
+      .gsub(/:/, "：")
+      .gsub(/\*/, "＊")
+      .gsub(/\|/, "｜")
+      .gsub(/"/, '”')
+      .gsub(/</, "＜")
+      .gsub(/>/, "＞")
       .byteslice(0, 200).scrub('') # safe length for filename
   end
 
