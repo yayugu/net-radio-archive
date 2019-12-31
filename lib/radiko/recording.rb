344b1f036eb46a7df11b6e10df3d0632c276e715
yayugu <yayugu@gmail.com>
2019-12-31 14:52:16 +0000

    support Radiko HLS

--- lib/radiko/recording.rb
+++ lib/radiko/recording.rb
@@ -5,8 +5,7 @@ require 'base64'
 
 module Radiko
   class Recording
-    SWF_URL = 'http://radiko.jp/apps/js/flash/myplayer-release.swf'
-    RTMP_URL = 'rtmpe://f-radiko.smartstream.ne.jp'
+    AUTH_KEY = 'bcd151073c03b352e1ef2fd66c32209da9ca0afa'
     WORK_DIR_NAME = 'radiko'
 
     def initialize
@@ -29,31 +28,17 @@ module Radiko
         Main::retry do
           auth(job)
         end
-        rtmp(job)
+				m3u8_chunk(job)
+        download_hls(job)
       ensure
         logout
       end
     end
 
     def auth(job)
-      dl_swf
-      extract_swf
       login(job)
       auth1
       auth2
-    end
-
-    def dl_swf
-      unless File.exists?(swf_path)
-        Main::download(SWF_URL, swf_path)
-      end
-    end
-
-    def extract_swf
-      unless File.exists?(key_path)
-        command = "swfextract -b 12 #{swf_path} -o #{key_path}"
-        Main::shell_exec(command)
-      end
     end
 
     def login(job)
@@ -79,84 +64,87 @@ module Radiko
     end
 
     def auth1
-      uri = URI('https://radiko.jp/v2/api/auth1_fms')
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      https.start do |h|
-        res = h.post(
-            uri.path,
-            '',
-            {
-                'pragma' => 'no-cache',
-                'X-Radiko-App' => 'pc_ts',
-                'X-Radiko-App-Version' => '4.0.0',
-                'X-Radiko-User' => 'test-stream',
-                'X-Radiko-Device' => 'pc',
-                'Cookie' => @cookie,
-            }
-        )
-        @auth_token = /x-radiko-authtoken=([\w-]+)/i.match(res.body)[1]
-        @offset = /x-radiko-keyoffset=(\d+)/i.match(res.body)[1].to_i
-        @length = /x-radiko-keylength=(\d+)/i.match(res.body)[1].to_i
-      end
+      uri = 'https://radiko.jp/v2/api/auth1'
+      res = HTTParty.get(uri, {
+        headers: {
+					'pragma' => 'no-cache',
+					'X-Radiko-App' => 'pc_html5',
+					'X-Radiko-App-Version' => '0.0.1',
+					'X-Radiko-User' => 'test-stream',
+					'X-Radiko-Device' => 'pc',
+					'Cookie' => @cookie,
+        }
+      })
+			@auth_token = res.headers['x-radiko-authtoken']
+			@offset = res.headers['x-radiko-keyoffset'].to_i
+			@length = res.headers['x-radiko-keylength'].to_i
     end
 
     def partialkey
-      open(key_path, 'rb:ASCII-8BIT') do |fp|
-        fp.seek(@offset)
-        return Base64.strict_encode64(fp.read(@length))
-      end
+      return Base64.strict_encode64(AUTH_KEY[@offset, @length])
     end
 
     def auth2
-      uri = URI('https://radiko.jp/v2/api/auth2_fms')
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      https.start do |h|
-        res = h.post(
-            uri.path,
-            '',
-            {
-                'pragma' => 'no-cache',
-                'X-Radiko-App' => 'pc_ts',
-                'X-Radiko-App-Version' => '4.0.0',
-                'X-Radiko-User' => 'test-stream',
-                'X-Radiko-Device' => 'pc',
-                'X-Radiko-Authtoken' => @auth_token,
-                'X-Radiko-Partialkey' => partialkey,
-                'Cookie' => @cookie,
-            }
-        )
-        @area_id = /^([^,]+),/.match(res.body)[1]
-      end
+      uri = 'https://radiko.jp/v2/api/auth2'
+      res = HTTParty.get(uri, {
+        headers: {
+					'pragma' => 'no-cache',
+					'X-Radiko-User' => 'test-stream',
+					'X-Radiko-Device' => 'pc',
+					'X-Radiko-AuthToken' => @auth_token,
+					'X-Radiko-PartialKey' => partialkey,
+					'Cookie' => @cookie,
+        }
+      })
+			p res
+			p res.body
     end
 
-    def rtmp(job)
+    def m3u8_chunk(job)
+      uri = "http://c-radiko.smartstream.ne.jp/#{job.ch}/_definst_/simul-stream.stream/playlist.m3u8"
+			p uri
+      res = HTTParty.get(uri, {
+        headers: {
+          'X-Radiko-Authtoken' => @auth_token
+        }
+      })
+      p res
+      @m3u8_url = /^https?:\/\/.+m3u8$/i.match(res.body)[1]
+    end
+
+    def download_hls(job)
       Main::sleep_until(job.start - 10.seconds)
 
       length = job.length_sec + 60
-      flv_path = Main::file_path_working(job.ch, title(job), 'flv')
-      command = "\
-        rtmpdump \
-          -r #{Shellwords.escape(RTMP_URL)} \
-          --playpath 'simul-stream.stream' \
-          --app '#{job.ch}/_definst_' \
-          -W #{SWF_URL} \
-          -C S:'' -C S:'' -C S:'' -C S:#{@auth_token} \
-          --live \
-          --stop #{length} \
-          -o #{Shellwords.escape(flv_path)} \
-        2>&1"
+      file_path = Main::file_path_working(job.ch, title(job), 'm4a')
+      arg = "\
+        -loglevel warning \
+        -headers 'X-Radiko-AuthToken: #{@auth_token}' \
+        -y \
+        -i #{Shellwords.escape(@m3u8_url)} \
+        -t #{length} \
+        -vcodec none -acodec copy -bsf:a aac_adtstoasc \
+        #{Shellwords.escape(file_path)}"
 
-      exit_status, output = Main::shell_exec(command)
-      unless exit_status.success?
+      exit_status, output = Main::ffmpeg(arg)
+      unless exit_status.success? && output.blank?
         Rails.logger.error "rec failed. job:#{job.id}, exit_status:#{exit_status}, output:#{output}"
         return false
       end
 
       true
+    end
+
+    def exec_convert(job)
+      m4a_path = Main::file_path_working(job.ch, title(job), 'm4a')
+      if Settings.force_mp4
+        mp4_path = Main::file_path_working(job.ch, title(job), 'mp4')
+        Main::convert_ffmpeg_to_mp4_with_blank_video(m4a_path, mp4_path, job)
+        dst_path = mp4_path
+      else
+        dst_path = m4a_path
+      end
+      Main::move_to_archive_dir(job.ch, job.start, dst_path)
     end
 
     def logout
@@ -181,31 +169,9 @@ module Radiko
       end
     end
 
-    def exec_convert(job)
-      flv_path = Main::file_path_working(job.ch, title(job), 'flv')
-      if Settings.force_mp4
-        mp4_path = Main::file_path_working(job.ch, title(job), 'mp4')
-        Main::convert_ffmpeg_to_mp4_with_blank_video(flv_path, mp4_path, job)
-        dst_path = mp4_path
-      else
-        m4a_path = Main::file_path_working(job.ch, title(job), 'm4a')
-        Main::convert_ffmpeg_to_m4a(flv_path, m4a_path, job)
-        dst_path = m4a_path
-      end
-      Main::move_to_archive_dir(job.ch, job.start, dst_path)
-    end
-
     def title(job)
       date = job.start.strftime('%Y_%m_%d_%H%M')
       "#{date}_#{job.title}"
-    end
-
-    def swf_path
-      Main::file_path_working_base(WORK_DIR_NAME, "player2.swf")
-    end
-
-    def key_path
-      Main::file_path_working_base(WORK_DIR_NAME, "radiko_key2.png")
     end
   end
 end
