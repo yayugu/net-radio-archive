98c8b627ccce03b866c2090148b1a3481948bdf3
hogewest <hogewest@gmail.com>
2020-10-29 00:50:05 +0900

    A&GのHLS対応

--- lib/ag/recording.rb
+++ lib/ag/recording.rb
@@ -3,39 +3,39 @@ require 'fileutils'
 
 module Ag
   class Recording
-    AGQR_STREAM_URL = 'rtmp://fms-base2.mitene.ad.jp/agqr/aandg333'
+    AGQR_STREAM_URL = 'https://fms2.uniqueradio.jp/agqr10/aandg1.m3u8'
     CH_NAME = 'ag'
 
     def record(job)
-      unless exec_rec(job)
-        return false
-      end
-      exec_convert(job)
-
-      true
+      exec_rec(job)
     end
 
     def exec_rec(job)
       Main::prepare_working_dir(CH_NAME)
       Main::sleep_until(job.start - 10.seconds)
 
-      length = job.length_sec + 60
-      flv_path = Main::file_path_working(CH_NAME, title(job), 'flv')
-      command = "rtmpdump -q -r #{Shellwords.escape(AGQR_STREAM_URL)} --live --stop #{length} -o #{Shellwords.escape(flv_path)}"
-      exit_status, output = Main::shell_exec(command)
+      length = job.length_sec + 90
+      file_path = Main::file_path_working(CH_NAME, title(job), 'mp4')
+      arg = "\
+        -loglevel error \
+        -y \
+        -allowed_extensions ALL \
+        -protocol_whitelist file,crypto,http,https,tcp,tls \
+        -i #{AGQR_STREAM_URL} \
+        -t #{length} \
+        -vcodec copy -acodec copy -bsf:a aac_adtstoasc \
+        #{Shellwords.escape(file_path)}"
+      exit_status, output = Main::ffmpeg(arg)
       unless exit_status.success?
         Rails.logger.error "rec failed. job:#{job}, exit_status:#{exit_status}, output:#{output}"
         return false
       end
+      if output.present?
+        Rails.logger.warn "ag ffmpeg command:#{arg} output:#{output}"
+      end
 
+      Main::move_to_archive_dir(CH_NAME, job.start, file_path)
       true
-    end
-
-    def exec_convert(job)
-      flv_path = Main::file_path_working(CH_NAME, title(job), 'flv')
-      mp4_path = Main::file_path_working(CH_NAME, title(job), 'mp4')
-      Main::convert_ffmpeg_to_mp4(flv_path, mp4_path, job)
-      Main::move_to_archive_dir(CH_NAME, job.start, mp4_path)
     end
 
     def title(job)
